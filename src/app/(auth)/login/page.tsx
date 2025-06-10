087e0707b7b1e9a29cd2a475e145dc50a13da1d3
Mehdy-H-Asaad <mehdyasaad.sy.2003@gmail.com>
2025-06-10 04:57:53 +0400

    minor changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 import { SectionTitle } from "@/components/common/SectionTitle";
 import { LoginForm } from "@/features/auth/components/LoginForm";
 
-export const page = () => {
+const page = () => {
 	return (
 		<div className="flex min-h-[calc(100svh-80px)] w-full items-center justify-center p-6 md:p-10">
 			<div className="w-full max-w-sm">
